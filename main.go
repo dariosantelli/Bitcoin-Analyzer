e312fcb83d949330bd03238c2b350589cf7e671f
Dario Santelli <dariosantelli@gmail.com>
2022-02-25 11:38:16 -0500

    Initial setup

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Testing")
+	var input_num string
+	fmt.Scanln(&input_num)
+
+	fmt.Println("Your input was: ", input_num)
+
+	// need to create command line interface
+	// get access to live BTC transactions
+}
