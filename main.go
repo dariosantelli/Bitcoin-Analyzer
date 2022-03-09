9762bae2f4c7a6d3b43f7b71b0bf37ff9112993e
Dario Santelli <dariosantelli@gmail.com>
2022-03-08 19:11:28 -0500

    Got zmq functionality somewhat working

--- main.go
+++ main.go
@@ -7,18 +7,45 @@ import (
 )
 
 func main() {
-	// context, _ := zmq4.NewContext()
-	socket, _ := zmq4.NewSocket(zmq4.SUB)
+	context, _ := zmq4.NewContext()
+	socket, _ := context.NewSocket(zmq4.SUB)
 	// defer context.Close()
+	defer context.Term()
 	defer socket.Close()
 
-	addr := "tcp://127.0.0.1:3000"
-
+	addr := "tcp://127.0.0.1:29000"
+	socket.SetSubscribe("rawtx")
 	socket.Connect(addr)
 
-	data, _ := socket.Recv(0)
+	for {
 
-	fmt.Println(data)
+		// received, _ := socket.Recv(0)
+
+		// fmt.Println("Raw received: ", received)
+
+		received, _ := socket.RecvMessage(0)
+
+		fmt.Println("Raw received: ", received)
+
+		/*
+			var received_bytes [][]byte
+			received_bytes, _ = socket.RecvMessageBytes(0)
+			// hex.Decode(bites, data)
+			// var dst []byte
+
+			// hex.Decode(dst, received_bytes)
+
+			fmt.Println("Decoded bytes: ", received_bytes)
+
+			var received_hex string
+			received_hex, _ = socket.Recv(0)
+
+			var temp_bytes []byte
+			temp_bytes, _ = hex.DecodeString(received_hex)
+
+			fmt.Println("Received data: ", temp_bytes)
+		*/
+	}
 
 	// need to create command line interface
 	// get access to live BTC transactions
