0b9241d6dc35b0ff60ff2bc73e61dff3035a0cea
Dario Santelli <dariosantelli@gmail.com>
2022-03-14 20:26:26 -0400

    Set up desired command line UI interactions

--- main.go
+++ main.go
@@ -1,15 +1,16 @@
 package main
 
 import (
+	"encoding/hex"
 	"fmt"
+	"os/exec"
 	"strconv"
 
-	"encoding/hex"
-
+	"github.com/eiannone/keyboard"
 	zmq4 "github.com/pebbe/zmq4"
 )
 
-func main() {
+func runZmq(quit chan bool) {
 	context, _ := zmq4.NewContext()
 	socket, _ := context.NewSocket(zmq4.SUB)
 	// defer context.Close()
@@ -21,33 +22,58 @@ func main() {
 	socket.Connect(addr)
 
 	for {
+		select {
+		case quitsignal := <-quit:
+			fmt.Println("Got signal: ", quitsignal)
+			return
+		default:
+			fmt.Println("Did not get quit signal")
+			received, _ := socket.RecvMessage(0)
 
-		// received, _ := socket.Recv(0)
+			fmt.Println("Hash received: ", received)
 
-		// fmt.Println("Raw received: ", received)
+			topic := received[0]
+			data := received[1]
+			count := received[2]
 
-		received, _ := socket.RecvMessage(0)
+			fmt.Println("\tTopic: ", topic, " | ", len(topic))
+			fmt.Println("\tData: ", hex.EncodeToString([]byte(data)), " | ", len(data))
+			fmt.Println("\tCount: ", hex.EncodeToString([]byte(count)), " | ", len(count))
 
-		fmt.Println("Hash received: ", received)
+			format, _ := strconv.ParseInt(count, 16, 8)
+			fmt.Println("Formatted Count: ", format)
 
-		topic := received[0]
-		data := received[1]
-		count := received[2]
+			fmt.Println("Data byte array: ", []byte(data))
 
-		fmt.Println("\tTopic: ", topic, " | ", len(topic))
-		fmt.Println("\tData: ", hex.EncodeToString([]byte(data)), " | ", len(data))
-		fmt.Println("\tCount: ", hex.EncodeToString([]byte(count)), " | ", len(count))
+			fmt.Println("Out: ", hex.EncodeToString([]byte(data)))
+			fmt.Println("Len of out: ", len(hex.EncodeToString([]byte(data))))
+		}
+	}
+}
 
-		format, _ := strconv.ParseInt(count, 16, 8)
-		fmt.Println("Formatted Count: ", format)
+func main() {
 
-		fmt.Println("Data byte array: ", []byte(data))
+	var input int = 0
+	fmt.Println("Please enter 1")
+	fmt.Scanln(&input)
 
-		fmt.Println("Out: ", hex.EncodeToString([]byte(data)))
-		fmt.Println("Len of out: ", len(hex.EncodeToString([]byte(data))))
+	quit := make(chan bool)
 
+	if input == 1 {
+		go runZmq(quit)
+		keystroke, _, _ := keyboard.GetSingleKey()
+		fmt.Println("Got keystroke: ", keystroke)
+		quit <- true
+	} else {
+		fmt.Println("Input was not 1")
 	}
 
+	var input2 int = 0
+	fmt.Println("Please enter another thing")
+	fmt.Scanln(&input2)
+
+	cmd := exec.Command("gnome-terminal", "-e", "/home/dariosantelli/Documents")
+	cmd.Start()
 	// need to create command line interface
 	// get access to live BTC transactions
 }
