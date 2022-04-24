b3743891f68e54ed45a0106c6fe90c5f45592927
Dario <dariosantelli@gmail.com>
2022-04-24 08:26:08 -0700

    Added bitcoincli float64 function

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"encoding/binary"
 	"encoding/hex"
 	"encoding/json"
 	"fmt"
@@ -23,9 +24,15 @@ func setupSocket(addr string, notification_type string) (*zmq4.Socket, *zmq4.Con
 }
 
 func getCurrentMempoolCount() (count int) {
-	mempool_info := runBtcCliCommand("getmempoolinfo")
+	mempool_info := runBtcCliCommandMap("getmempoolinfo")
 
 	return int((mempool_info["size"]).(float64))
+}
+
+func getCurrentBlockCount() (count float64) {
+	// mempool_info := runBtcCliCommandFloat64("getblockcount")
+
+	return 1098
 }
 
 func listenForHashtx(updates chan string) {
@@ -78,17 +85,73 @@ func listenForHashtxWorker(socket *zmq4.Socket, num_transactions *int, output_en
 	}
 }
 
-func startZmq() (hashtx chan string) {
+func listenForHashblock(updates chan string) {
+
+	var socket, context = setupSocket("tcp://127.0.0.1:29002", "hashblock")
+	defer context.Term()
+	defer socket.Close()
+
+	var num_blocks float64 = getCurrentBlockCount()
+	var output_enabled bool = false
+
+	go listenForHashBlockWorker(socket, &num_blocks, &output_enabled)
+
+	for {
+		input := <-updates
+		switch input {
+		case "quit":
+			fmt.Println("Got quit case: ", input)
+			return
+		case "help":
+			fmt.Println("Got case help: ", input)
+		case "trans":
+			updates <- fmt.Sprint(num_blocks)
+		case "reset":
+			num_blocks = 0
+		case "enable":
+			output_enabled = true
+		case "disable":
+			output_enabled = false
+		case "test":
+			fmt.Println("# of current blocks: ", num_blocks)
+		default:
+			fmt.Println("Got case default: ", input)
+
+			// fmt.Println("Default")
+
+		}
+
+	}
+}
+
+func listenForHashBlockWorker(socket *zmq4.Socket, num_blocks *float64, output_enabled *bool) {
+
+	for {
+		received, _ := socket.RecvMessage(0)
+		data := received[1]
+		if *output_enabled {
+			fmt.Println("Received: ", hex.EncodeToString([]byte(data)))
+		}
+
+		*num_blocks += 1
+	}
+}
+
+func startZmq() (hashtx chan string, hashblock chan string) {
 	hashtx_updates := make(chan string)
 	go listenForHashtx(hashtx_updates)
 
-	return hashtx_updates
+	hashblock_updates := make(chan string)
+	go listenForHashblock(hashblock_updates)
+
+	return hashtx_updates, hashblock_updates
 }
 
 func runMainMenu() {
-	fmt.Println("(1) Do first thing")
+	fmt.Println("(1) Current mempool count")
 	fmt.Println("(2) Do second thing")
 	fmt.Println("(3) Tx Menu")
+	fmt.Println("(9) Exit")
 	fmt.Println("\nPlease select an entry")
 }
 
@@ -99,6 +162,7 @@ func runTxMenu(hash_updates chan string) {
 		fmt.Println("(2) Reset Txs")
 		fmt.Println("(3) Enable live output")
 		fmt.Println("(4) Disable live output")
+		fmt.Println("(9) Exit")
 		fmt.Println("\nPlease select an entry")
 		input, _, _ := keyboard.GetSingleKey()
 		switch input {
@@ -122,7 +186,9 @@ func runTxMenu(hash_updates chan string) {
 
 func main() {
 
-	hash_updates := startZmq()
+	hashtx_updates, hashblock_updates := startZmq()
+
+	fmt.Println("Command result: ", runBtcCliCommandFloat64("getblockcount"))
 
 	for {
 		runMainMenu()
@@ -131,26 +197,26 @@ func main() {
 
 		//convert key # to key value
 
-		//command line parser to plug in bitcoin.conf path
+		// command line parser to plug in bitcoin.conf path
+		// update tx count when new block arrives
 
 		switch input {
 		case 49: //1
-			fmt.Println("you selected 1")
-			fmt.Println("count: ", getCurrentMempoolCount())
+			fmt.Println("Current mempool count: ", getCurrentMempoolCount())
 		case 50: //2
-			fmt.Println("two was selected")
+			hashblock_updates <- "test"
 		case 51: //3
-			runTxMenu(hash_updates)
+			runTxMenu(hashtx_updates)
 		case 52: //4
-			hash_updates <- "reset"
+			hashtx_updates <- "reset"
 		case 53: //5
-			hash_updates <- "enable"
+			hashtx_updates <- "enable"
 		case 54: //6
-			hash_updates <- "disable"
+			hashtx_updates <- "disable"
 		case 57:
 			return
 		default:
-			fmt.Println("Invalid entry")
+			fmt.Println("Please select a valid option")
 		}
 	}
 
@@ -158,7 +224,7 @@ func main() {
 	// get access to live BTC transactions
 }
 
-func runBtcCliCommand(command string) (output map[string]interface{}) {
+func runBtcCliCommandMap(command string) (output map[string]interface{}) {
 
 	var command_to_run = bitcoin_conf_path + " " + command
 
@@ -178,4 +244,39 @@ func runBtcCliCommand(command string) (output map[string]interface{}) {
 
 	return result
 
+}
+
+func runBtcCliCommandFloat64(command string) (output float64) {
+	var command_to_run = bitcoin_conf_path + " " + command
+
+	cmd := exec.Command("bash", "-c", command_to_run)
+
+	var byte_buffer bytes.Buffer
+	// var result float64
+
+	cmd.Stdout = &byte_buffer
+
+	_ = cmd.Run()
+
+	fmt.Println("Bytes out: ", byte_buffer)
+
+	result, _ := byte_buffer.ReadBytes(0)
+
+	test := bytes.NewReader(result)
+
+	var test_result_BE float64
+	var test_result_LE float64
+
+	binary.Read(test, binary.BigEndian, test_result_BE)
+	binary.Read(test, binary.LittleEndian, test_result_LE)
+
+	fmt.Println("Result in string: ", hex.EncodeToString(result))
+	fmt.Println("Result output BE: ", &test_result_BE)
+	fmt.Println("Result output LE: ", &test_result_LE)
+
+	// hex.Decode()
+
+	floatresult := float64(result[6])
+
+	return floatresult
 }
